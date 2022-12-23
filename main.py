904f81e7d8c91b4fd43a76aff8fefd5b64d4814d
n4okins <naoki.jsdf.jj@gmail.com>
2022-12-23 14:31:51 +0900

    a

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import jax
+import jax.numpy as jnp
+import jraph
+import evojax
+from evojax.task.base import TaskState, VectorizedTask
+from evojax.policy.base import PolicyState, PolicyNetwork
+
+from torchvision import datasets
+from flax import linen as nn
+from flax.struct import dataclass
+import numpy as np
+
 
 if __name__ == "__main__":
     print(jax.default_backend())
