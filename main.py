2a3801a55689629649baa6438edd2651538a1806
n4okins <naoki.jsdf.jj@gmail.com>
2022-12-26 22:30:41 +0900

    Add Forward-Forward Algorithm

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 import jax
-import jax.numpy as jnp
-import jraph
-import evojax
-from evojax.task.base import TaskState, VectorizedTask
-from evojax.policy.base import PolicyState, PolicyNetwork
-
-from torchvision import datasets
-from flax import linen as nn
-from flax.struct import dataclass
-import numpy as np
-
 
 if __name__ == "__main__":
     print(jax.default_backend())
